7c94ff5b63003dc64a69a1460390d788660ab00b
laura mairot <lauramairot@gmail.com>
2020-05-13 19:27:32 +0200

    validations

--- db/migrate/20200511201307_add_restaurant_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddRestaurantToReviews < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :reviews, :restaurant, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_185352) do
+ActiveRecord::Schema.define(version: 2020_05_13_171934) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -23,11 +23,13 @@ ActiveRecord::Schema.define(version: 2020_05_11_185352) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.text "content"
+    t.string "content"
     t.integer "rating"
-    t.string "references"
+    t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
